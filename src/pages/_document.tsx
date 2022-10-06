ea85f87349faff5afbd0e4e7d0ce21e5d1c8593d
Lincoln Dias Marques <licodmarques@gmail.com>
2022-10-06 10:11:49 -0300

    fix: meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt-br">
       <Head>
+        <meta
+          name="description"
+          content="App feito pelo Ignite da Rocketseat."
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
