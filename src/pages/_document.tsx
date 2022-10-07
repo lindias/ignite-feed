e4773ea4da990c06822327c9b7f81942216895f7
Lincoln Dias Marques <licodmarques@gmail.com>
2022-10-07 13:24:33 -0300

    fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,24 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <meta
-          name="description"
-          content="App feito pelo Ignite da Rocketseat."
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta
+            name="description"
+            content="App feito pelo Ignite da Rocketseat."
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
